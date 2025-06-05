9c808724ec5ec9993ab15107a4f26a14d5d0d05a
s1d79 <ezwandey@gmail.com>
2025-06-05 19:57:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,3 +117,9 @@ def evaluate():
 
 if __name__ == "__main__":
     app.run()
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
